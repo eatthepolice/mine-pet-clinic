298b3cce5180d5418c72bd2e10c81955624ad7aa
eatthepolice <moskaluklev@gmail.com>
2022-08-03 00:58:37 +0200

    Start

---
+++ src/main/java/webapp/springframework/petclinic/MinePetClinicApplication.java
@@ -0,0 +1,13 @@
+package webapp.springframework.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinePetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinePetClinicApplication.class, args);
+	}
+
+}
